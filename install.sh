28b82c11fd7aff5661f619f6874fcd1398824fb6
Taylor Briggs <taylor.briggs@gmail.com>
2017-07-26 16:53:40 -0500

    Improve install script to link bundle and nvim config files

--- install.sh
+++ install.sh
@@ -2,70 +2,68 @@
 
 cutstring="DO NOT EDIT BELOW THIS LINE"
 
-copyDir() {
-  cp -Rn "$1" "$2"
-}
-
 linkFile() {
   ln -s "$1" "$2"
 }
 
-for dir in */; do
-  target="$HOME/.$dir"
+walkFolderAndLinkContents () {
+  maybe_folder=$1
 
-  if [ -e $target ]; then
-    for name in $dir*; do
-      inner_target="$target${name#$dir}"
+  for file in $maybe_folder*; do
+    if [ -f $file ]; then
+      filename=${file#$maybe_folder}
+      dest="$HOME/.$maybe_folder"
 
-      if [ ! -e "$inner_target" ]; then
-        echo "Creating $inner_target"
-
-        if [ -d $name ]; then
-          copyDir "$PWD/$name" "$inner_target"
-        else
-          linkFile "$PWD/$name" "$inner_target"
-        fi
+      if [ -e "$dest$filename" ]; then
+        echo "$dest$filename already exists!"
+      else
+        echo "Creating $dest$filename..."
+        linkFile "$PWD/$file" "$dest"
       fi
-    done
-  else
-    echo "Creating $target"
+    elif [ -d $file ]; then
+      walkFolderAndLinkContents $file/
+    else
+      echo "Nothing to see here..."
+    fi
+  done
+}
 
-    copyDir "$PWD/$dir" "$target"
-  fi
+for dir in */; do
+  walkFolderAndLinkContents $dir
 done
 
 for name in *; do
   target="$HOME/.$name"
 
   if [ -f $name ]; then
-      if [[ $name != 'install.sh' ]]; then
-        if [ -e $target ]; then
-          if [ ! -L $target ]; then
-            cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
+    if [[ $name != 'install.sh' ]]; then
+      if [ -e $target ]; then
+        if [ ! -L $target ]; then
+          cutline=`grep -n -m1 "$cutstring" "$target" | sed "s/:.*//"`
 
-            if [[ -n $cutline ]]; then
-              let "cutline = $cutline - 1"
-              echo "Updating $target"
-              head -n $cutline "$target" > update_tmp
-              startline=`tail -r "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
-              if [[ -n $startline ]]; then
-                tail -n $startline "$name" >> update_tmp
-              else
-                cat "$name" >> update_tmp
-              fi
-              mv update_tmp "$target"
+          if [[ -n $cutline ]]; then
+            let "cutline = $cutline - 1"
+            echo "Updating $target"
+            head -n $cutline "$target" > update_tmp
+            startline=`tail -r "$name" | grep -n -m1 "$cutstring" | sed "s/:.*//"`
+            if [[ -n $startline ]]; then
+              tail -n $startline "$name" >> update_tmp
             else
-              echo "WARNING: $target exists but is not a symlink."
+              cat "$name" >> update_tmp
             fi
-          fi
-        else
-          echo "Creating $target"
-          if [[ -n `grep "$cutstring" "$name"` ]]; then
-            cp "$PWD/$name" "$target"
+            mv update_tmp "$target"
           else
-            linkFile "$PWD/$name" "$target"
+            echo "WARNING: $target exists but is not a symlink."
           fi
         fi
+      else
+        echo "Creating $target"
+        if [[ -n `grep "$cutstring" "$name"` ]]; then
+          cp "$PWD/$name" "$target"
+        else
+          linkFile "$PWD/$name" "$target"
+        fi
       fi
+    fi
   fi
 done
