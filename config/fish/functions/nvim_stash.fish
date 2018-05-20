a218483a8abaf04012eb749da2b27c1d554b3c59
Taylor Briggs <taylor.briggs@gmail.com>
2018-05-20 17:21:16 -0500

    Fix nvim_stash function

--- config/fish/functions/nvim_stash.fish
+++ config/fish/functions/nvim_stash.fish
@@ -1,4 +1,4 @@
 function nvim_stash
-	mv $HOME/dotfiles/config/fish/nvim $HOME/dotfiles/config/fish/xnvim
+	mv $HOME/dotfiles/config/nvim $HOME/dotfiles/config/xnvim
   mkdir $HOME/dotfiles/config/nvim
 end
