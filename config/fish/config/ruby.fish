a2f647abeaf1f918f3a8a3cdc268a63bc5f19b6c
Taylor Briggs <taylor.briggs@cobalt.io>
2023-05-10 16:49:30 -0500

    Fix ruby config for ruby 3.2.2

--- config/fish/config/ruby.fish
+++ config/fish/config/ruby.fish
@@ -1,1 +1,1 @@
-set -q RUBY_CONFIGURE_OPTS; or set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix)/opt/openssl@1.1"
+set -q RUBY_CONFIGURE_OPTS; or set -gx RUBY_CONFIGURE_OPTS "--with-openssl-dir=$(brew --prefix openssl@3)"
