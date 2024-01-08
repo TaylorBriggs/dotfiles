a9849df74eaa5357e40f479485269478301f1746
Taylor Briggs <taylor.briggs@cobalt.io>
2024-01-08 13:17:30 -0600

    Transition from rtx to mise

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,9 @@
 # this loads homebrew variables
 /opt/homebrew/bin/brew shellenv | source
-# rtx
-rtx activate fish | source
 # other configs
 source $HOME/.config/fish/config/gpg.fish
 source $HOME/.config/fish/config/github.fish
 source $HOME/.config/fish/config/ruby.fish
 source $HOME/.config/fish/config/editor.fish
+
+string match -q "$TERM_PROGRAM" "vscode" and . (code --locate-shell-integration-path fish)
