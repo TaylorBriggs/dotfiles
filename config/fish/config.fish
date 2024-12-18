5791840ba9cb23b701f4d9afa85356e470d34cb4
Taylor Briggs <taylor.briggs@cobalt.io>
2024-12-18 13:19:05 -0600

    Make sure correct psql version is set in PATH

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,6 +3,7 @@
 # other configs
 source $HOME/.config/fish/config/gpg.fish
 source $HOME/.config/fish/config/github.fish
+source $HOME/.config/fish/config/postgresql.fish
 source $HOME/.config/fish/config/ruby.fish
 source $HOME/.config/fish/config/editor.fish
 source $HOME/.config/fish/config/terminal.fish
---
+++ config/fish/config/postgresql.fish
@@ -0,0 +1,2 @@
+set -l POSTGRES_VERSION 15
+fish_add_path (brew --prefix postgresql@$POSTGRES_VERSION)/bin
